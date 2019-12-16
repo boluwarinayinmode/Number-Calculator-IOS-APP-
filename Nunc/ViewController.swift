5f10896e8a02986a7d9225afe99d1e3e9a640373
Boluwarin Ayinmode <Boluwarin@boluwarin-2.local>
2019-12-16 16:10:51 +0100

    Initial Commit

---
+++ Nunc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nunc
+//
+//  Created by Boluwarin Ayinmode on 12/16/19.
+//  Copyright © 2019 Boluwarin Ayinmode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
